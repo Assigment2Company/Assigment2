701cec9e55d3f1987c00eea745d4ea371ba2ed29
David Bardina <bardina.dev@gmail.com>
2024-01-08 01:40:18 +0100

    Add Serializer as Singleton

--- Engine/Source/Serializer.cpp
+++ Engine/Source/Serializer.cpp
@@ -1,6 +1,25 @@
 #pragma once
 
 class Serializer {
+private:
 
+	static Serializer* serializer;
+
+	Serializer() 
+	{
+	}
+
+public:
+	// deleting copy constructor
+	Serializer(const Serializer& obj) = delete;
+
+	static Serializer* getInstance()
+	{
+		if (serializer == nullptr)
+		{
+			serializer = new Serializer();
+		}
+
+		return serializer;
+	}
 };
-
